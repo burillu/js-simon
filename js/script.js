ad4422cebf8c85f0f4907774bd5f9695c0cc5538
burillu <burillu90@hotmail.it>
2023-10-20 17:36:44 +0200

    add input for numbers, fn onchechnge

--- js/script.js
+++ js/script.js
@@ -11,8 +11,8 @@ btnPlay.addEventListener('click', playGame);
 let randNumArray = rndDifferentNumb();
 console.log(randNumArray);
 
-// timeout di 30 secondi
-//prendo input dell’utente
+
+
 //ciclo sull’array
 //quando trovo un numero uguale lo metto in una nuova array
 //length mi da il punteggio
@@ -20,9 +20,13 @@ console.log(randNumArray);
 
 //funzione playGame
 function playGame() {
+    //mostro i numeri per 30 secondi
     console.log('Ecco i 5 numeri. \n Hai 30s per memorizzarli :' + randNumArray);
+    // timeout di 30 secondi
     const number1 = setTimeout(yourInput, 5000);
     //clearTimeout(number1);
+    //prendo input dell’utente con funzione
+    let inputUser = [1,22,58,41,5]; //inputReader()
 
 
 }
@@ -45,9 +49,28 @@ function rndDifferentNumb() {
 }
 // funzione stampare i numeri a schermo
 function yourInput(){
-    //element = document.getElementById(target);
+    let input = document.createElement(input);
     console.clear();
     console.log('adesso è il tuo turno, inserisci i numeri');
-    //faccio apparire input
+    //faccio apparire input con un listener
     //al click del bottone leggo il valore
+    
 }
+//leggo valore inserito in input
+function inputReader(){
+
+}
+//stampo numeri a schermo
+function printNumb(){
+
+}
+// funzione per passare da un input all altro
+function onchechnge(i){
+    let dom = document.getElementById("key"+i);
+    let ml = dom.maxLength;
+    let lg = dom.value.length;
+    if (lg >= ml) {
+        document.getElementById("key"+(i+1)).focus()
+    }
+}
+// onchechnge();
